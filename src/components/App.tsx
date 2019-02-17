45509bbb87eefbad1beec1f832e04141d4df557b
Eduardo Velasco <eduardo@ordermygear.com>
2019-02-17 00:09:31 -0600

    basic login ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,33 @@
 import * as React from "react";
-import Grid from "./Grid";
-import Bar from "./Bar";
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Dashboard from "./Dashboard";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import AuthBar from "./AuthBar";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-        main: "#353A48",
+      main: "#353A48"
     },
     secondary: {
-      main: '#89B448',
-    },
-  },
+      main: "#89B448"
+    }
+  }
 });
 
 export default class App extends React.Component<any, any> {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <Bar />
-        <Grid />
-      </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <AuthBar />
+          <Route path="/sign-in" exact component={SignIn} />
+          <Route path="/sign-up" exact component={SignUp} />
+          <Route path="/" exact component={Dashboard} />
+        </MuiThemeProvider>
+      </Router>
     );
   }
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Grid from "./Grid";
+import DashboardBar from "./DashboardBar";
+
+export default () => (
+  <React.Fragment>
+    <DashboardBar />
+    <Grid />
+  </React.Fragment>
+);
